6f767a1c88db28304b6e06caaf573358fd561575
KseniaShumanskaya <KseniaShumanskaya@gmail.com>
2021-11-15 22:19:01 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//#include "fraction.h"
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//    Fraction fraction(-45,5);
+//    Fraction fraction2(8, 784);
+//    cout << fraction.GetNumerator() << endl;
+//    cout << fraction.GetDenumerator() << endl;
+//    fraction.WriteFraction();
+//    cout << endl;
+//    fraction2.WriteFraction();
+//    cout << endl;
+//    cout << "sum: ";
+//    fraction.operator+(fraction2).WriteFraction();
+//    cout << endl << "multiplication: ";
+//    fraction.operator*(fraction2).WriteFraction();
+//    cout << endl << "division: ";
+//    fraction.operator/(fraction2).WriteFraction();
+//    return 0;
+//}
